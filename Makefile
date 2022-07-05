ef13fcacc71258108122bbabdb5c173d0a2f73ef
svyatoslavxx <svyatoslavxx@yandex.ru>
2022-07-05 15:17:00 +0300

    завершение шага 4 с двумя неисправимыми ошибками линтера

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ node:
 	bin/brain-games.js
 publish:
 	npm publish --dry-run
+lint:
+	npx eslint .
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
+import userName from '/home/linux/frontend-project-lvl1/bin/src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+userName();
