4b8157864442c4f54aeaa037786862446fbefb9d
svyatoslavxx <svyatoslavxx@yandex.ru>
2022-07-08 15:24:19 +0300

    добавил запятую после хеллоу в cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const userName = () => {
   const respondentName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${respondentName}!`);
+  console.log(`Hello, ${respondentName}!`);
 };
 
 export default userName;
