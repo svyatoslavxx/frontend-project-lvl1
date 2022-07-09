f36a133d3ae229453e53af6cc0d8b139536a355e
svyatoslavxx <svyatoslavxx@yandex.ru>
2022-07-09 11:02:26 +0300

    вынес вычисления в отдельную функцию, добавил switch

--- src/games/brainCalc.js
+++ src/games/brainCalc.js
@@ -1,22 +1,26 @@
 import checkInputAndGiveBaseLogic from '../index.js';
+import getRandomNum from '../getRandomNum.js';
 
 const calculateNumbers = () => {
   const taskDescription = 'What is the result of the expression?';
   const calculateForBaseLogic = () => {
     const operatorList = ['+', '-', '*'];
-    const operator = operatorList[Math.round(Math.random(0, 2))];
-    const firstOperand = Math.round(Math.random() * 100);
-    const secondOperand = Math.round(Math.random() * 100);
+    const operator = operatorList[getRandomNum(0, 2)];
+    const firstOperand = getRandomNum(0, 100);
+    const secondOperand = getRandomNum(0, 100);
     let result = 0;
     const question = `${firstOperand} ${operator} ${secondOperand}`;
-    if (operator === '+') {
-      result = firstOperand + secondOperand;
-    }
-    if (operator === '-') {
-      result = firstOperand - secondOperand;
-    }
-    if (operator === '*') {
-      result = firstOperand * secondOperand;
+    switch (operator) {
+      case '+':
+        result = firstOperand + secondOperand;
+        break;
+      case '-':
+        result = firstOperand - secondOperand;
+        break;
+      case '*':
+        result = firstOperand * secondOperand;
+        break;
+      default: Error('Error');
     }
     result = result.toString();
     return [question, result];
