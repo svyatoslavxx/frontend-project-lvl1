1f0115c988e4f2510262ff3017fc944ede44f676
svyatoslavxx <svyatoslavxx@yandex.ru>
2022-07-10 23:03:21 +0300

    убрал лишнюю переменную

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -5,8 +5,7 @@ import startGame from '../index.js';
 const isNumberPrime = () => {
   const numberForQuestion = getRandomNumber(0, 100);
   const correctAnswer = checkIfPrime(numberForQuestion) ? 'yes' : 'no';
-  const result = [numberForQuestion, correctAnswer];
-  return result;
+  return [numberForQuestion, correctAnswer];
 };
 const startBrainPrimeGame = () => {
   const taskDescription = 'Answer "yes" if given number is prime. Otherwise answer "no"';
