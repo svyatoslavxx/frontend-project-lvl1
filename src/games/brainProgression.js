68600aac23a0ab3572a3785d987f730443976a3a
svyatoslavxx <svyatoslavxx@yandex.ru>
2022-07-10 23:06:00 +0300

    разбил на абстракции

--- src/games/brainProgression.js
+++ src/games/brainProgression.js
@@ -1,24 +1,24 @@
 import getRandomNumber from '../getRandomNumber.js';
 import startGame from '../index.js';
 
+const createProgression = () => {
+  const firstNumberInProgression = getRandomNumber(0, 100);
+  const step = getRandomNumber(2, 10);
+  const progressionLength = getRandomNumber(5, 9);
+  const hiddenElementIndex = getRandomNumber(0, progressionLength);
+  const numbersForQuestion = [];
+  numbersForQuestion.push(firstNumberInProgression + step);
+  for (let i = 1; i < progressionLength + 1; i += 1) {
+    numbersForQuestion.push(numbersForQuestion[i - 1] + step);
+  }
+  const correctHiddenElement = numbersForQuestion[hiddenElementIndex].toString();
+  const question = [...numbersForQuestion];
+  question[hiddenElementIndex] = '..';
+  const questionWithHiddenElement = question.join(' ');
+  return [questionWithHiddenElement, correctHiddenElement];
+};
 const startBrainProgressionGame = () => {
   const gameDescription = 'What number is missing in the progression?';
-  const progressionLength = getRandomNumber(5, 9);
-  const createProgression = () => {
-    const firstNumberInProgression = getRandomNumber(0, 100);
-    const step = getRandomNumber(2, 10);
-    const hiddenElementIndex = getRandomNumber(0, progressionLength);
-    const numbersForQuestion = [];
-    numbersForQuestion.push(firstNumberInProgression + step);
-    for (let i = 1; i < progressionLength + 1; i += 1) {
-      numbersForQuestion.push(numbersForQuestion[i - 1] + step);
-    }
-    const correctHiddenElement = numbersForQuestion[hiddenElementIndex].toString();
-    const question = [...numbersForQuestion];
-    question[hiddenElementIndex] = '..';
-    const questionWithHiddenElement = question.join(' ');
-    return [questionWithHiddenElement, correctHiddenElement];
-  };
   startGame(gameDescription, createProgression);
 };
 
